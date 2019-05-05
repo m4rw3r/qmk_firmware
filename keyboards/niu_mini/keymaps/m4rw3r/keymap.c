c81d82203d588ddaad2c844f47705c13820076eb
Martin Wernstål <m4rw3r@gmail.com>
2019-05-05 17:59:21 +0200

    m4rw3r: Keymap for NIU Mini

---
+++ keyboards/niu_mini/keymaps/m4rw3r/keymap.c
@@ -0,0 +1,33 @@
+#include QMK_KEYBOARD_H
+#include "m4rw3r.h"
+#include "m4rw3r_layouts.h"
+
+#define LAYOUT_planck_grid_wrapper(...) LAYOUT_ortho_4x12(__VA_ARGS__)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+[_QWERTY]     = LAYOUT_planck_grid_wrapper(M_QWERTY,  M_GRID_R5),
+[_COLEMAK]    = LAYOUT_planck_grid_wrapper(M_COLEMAK, M_GRID_R5),
+[_DVORAK]     = LAYOUT_planck_grid_wrapper(M_DVORAK,  M_GRID_R5),
+// TODO: Needs better placeholder
+[_GAME]       = LAYOUT_planck_grid_wrapper(M_QWERTY,  M_GAME_R5),
+[_KEYPAD]     = LAYOUT_planck_grid_wrapper(M_QWERTY,  M_GRID_R5),
+// We populate the game layer just in case
+[_GAME_LOWER] = LAYOUT_planck_grid_wrapper(M_LOWER_R2, M_LOWER_R3, M_LOWER_R4, M_GAME_R5),
+[_LOWER]      = LAYOUT_planck_grid_wrapper(M_LOWER_R2, M_LOWER_R3, M_LOWER_R4, M_LOWER_R5),
+[_RAISE]      = LAYOUT_planck_grid_wrapper(M_RAISE_R2, M_RAISE_R3, M_RAISE_R4, M_RAISE_R5),
+// Adjust layer is similar to planck/preonic, but with RGB controls instead of audio, and game layer toggle is removed
+[_ADJUST]     = LAYOUT_planck_grid_wrapper( \
+  _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL,  \
+  _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, AG_NORM, AG_SWAP, QWERTY,  _______, _______, _______, _______, \
+  _______, BL_TOGG, BL_STEP, _______, _______, _______, _______, _______, _______, _______, KEYPAD,  _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+),
+};
+
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
+  return true;
+}
+
+uint32_t layer_state_set_keymap(uint32_t state) {
+  return state;
+}
---
+++ keyboards/niu_mini/keymaps/m4rw3r/rules.mk
@@ -0,0 +1,6 @@
+# Required to toggle alt/win keys
+BOOTMAGIC_ENABLE = yes
+# Save a lot of space since we do not have a speaker
+AUDIO_ENABLE = no
+# LTO to optimize away unused stuff
+EXTRAFLAGS        += -flto
