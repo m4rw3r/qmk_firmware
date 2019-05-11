439b03d4e6fc782b62b6d6ef4b7b3e62e49aef65
Martin Wernstål <m4rw3r@gmail.com>
2019-05-11 15:09:42 +0200

    NIU Mini: Use LINK_TIME_OPTIMIZATION_ENABLE instead of adding it manually

--- keyboards/niu_mini/keymaps/m4rw3r/rules.mk
+++ keyboards/niu_mini/keymaps/m4rw3r/rules.mk
@@ -3,4 +3,4 @@ BOOTMAGIC_ENABLE = yes
 # Save a lot of space since we do not have a speaker
 AUDIO_ENABLE = no
 # LTO to optimize away unused stuff
-EXTRAFLAGS += -flto
+LINK_TIME_OPTIMIZATION_ENABLE = yes
