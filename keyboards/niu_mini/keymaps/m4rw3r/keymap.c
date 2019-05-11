4b77c9b35a906fab74f98ce020bc3f8e1e30e25d
Martin Wernstål <m4rw3r@gmail.com>
2019-05-11 20:02:11 +0200

    m4rw3r: Added underglow layer indicators

--- keyboards/niu_mini/keymaps/m4rw3r/keymap.c
+++ keyboards/niu_mini/keymaps/m4rw3r/keymap.c
@@ -19,9 +19,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE]      = LAYOUT_planck_grid_wrapper(M_RAISE_R2, M_RAISE_R3, M_RAISE_R4, M_RAISE_R5),
 // Adjust layer is similar to planck/preonic, but with RGB controls instead of audio, and game layer toggle is removed
 [_ADJUST]     = LAYOUT_planck_grid_wrapper( \
-  _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL,  \
-  _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, MAC_OFF, MAC_ON,  QWERTY,  COLEMAK, DVORAK,  _______, _______, \
-  _______, BL_TOGG, BL_STEP, _______, _______, _______, _______, _______, KEYPAD,  _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+  M_ADJUST_R2, \
+  RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, MAC_OFF, MAC_ON,  QWERTY,  COLEMAK, DVORAK,  _______, _______, \
+  RGBLAYER,RGB_RMOD,RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______, KEYPAD,  _______, _______, _______, \
+  BL_TOGG, BL_STEP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 ),
 };
--- users/m4rw3r/rgb_stuff.c
+++ users/m4rw3r/rgb_stuff.c
@@ -1,20 +1,68 @@
 #include "rgb_stuff.h"
 
+extern user_config_t user_config;
+
+uint8_t base_hue;
+uint8_t base_mode;
+
+/**
+ * Sets the RGB lights to solid colour of given hue without modifying
+ * brightness or saturation.
+ */
+void rgb_set_solid_hue(uint8_t hue) {
+  rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+  rgblight_sethsv_noeeprom(hue, rgblight_get_sat(), rgblight_get_val());
+}
+
 void layer_state_set_rgb(uint32_t state) {
-  // FIXME: Set colours
-  switch(biton32(state)) {
+  // Store base RGBlight settings, make sure to do this before we exit
+  switch(biton32(layer_state)) {
   case _GAME:
   case _RAISE:
   case _GAME_LOWER:
   case _LOWER:
   case _ADJUST:
-  case _MAC:
   case _KEYPAD:
+    // Do not save if we are not on a base-layer
     break;
+  default:
+    base_mode = rgblight_get_mode();
+    base_hue  = rgblight_get_hue();
+  }
+
+  if( ! user_config.use_rgb_layer_indicators) {
+    return;
+  }
+
+  switch(biton32(state)) {
+  case _GAME:
+    // Red
+    rgb_set_solid_hue(0);
+    break;
+  case _RAISE:
+    // Orange, matches button colour on Acute keycaps
+    rgb_set_solid_hue(28);
+    break;
+  case _GAME_LOWER:
+  case _LOWER:
+    // Magenta, matches button colour on Acute keycaps
+    rgb_set_solid_hue(213);
+    break;
+  case _ADJUST:
+    // Green
+    rgb_set_solid_hue(85);
+    break;
+  case _KEYPAD:
+    // Teal
+    rgb_set_solid_hue(128);
+    break;
+  default:
+    // Reset to user configuration, does not work
+    rgblight_sethsv_noeeprom(base_hue, rgblight_get_sat(), rgblight_get_val());
+    rgblight_mode_noeeprom(base_mode);
   }
 }
 
 void eeconfig_init_rgb(void) {
-  // rgblight_enable_noeeprom();
-  // TODO: Update stuff according to layers
+  rgblight_enable();
 }
