594ad1829242253d4367a31eb23e3f7c876d099b
Martin Wernstål <m4rw3r@gmail.com>
2019-05-12 12:02:43 +0200

    m4rw3r: Moved DEFAULT_LAYER_SONGS into config.h to make sure they get picked up

--- users/m4rw3r/config.h
+++ users/m4rw3r/config.h
@@ -4,3 +4,13 @@
 // We want to be able to do ESC : quickly, this will ignore the CTRL fucntion
 // of ESC if we release it before we release the other button
 #define IGNORE_MOD_TAP_INTERRUPT
+
+// Global audio configuration
+#ifdef AUDIO_ENABLE
+  #define STARTUP_SONG SONG(PREONIC_SOUND)
+
+  #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
+                                SONG(COLEMAK_SOUND), \
+                                SONG(DVORAK_SOUND) \
+                              }
+#endif
