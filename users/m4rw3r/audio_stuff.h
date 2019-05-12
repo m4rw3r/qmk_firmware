0291656f0006f137c31aa399a6b0825c01da951c
Martin Wernstål <m4rw3r@gmail.com>
2019-05-12 12:02:43 +0200

    m4rw3r: Moved DEFAULT_LAYER_SONGS into config.h to make sure they get picked up

--- users/m4rw3r/audio_stuff.h
+++ users/m4rw3r/audio_stuff.h
@@ -4,13 +4,6 @@
 #include "m4rw3r.h"
 #include "audio.h"
 
-#define STARTUP_SONG SONG(PREONIC_SOUND)
-
-#define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
-                                SONG(COLEMAK_SOUND), \
-                                SONG(DVORAK_SOUND) \
-                            }
-
 void layer_state_set_audio(uint32_t state);
 
 #endif
