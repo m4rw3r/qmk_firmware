cf7260e28b619d072d6a3addb34235d655967682
Martin Wernstål <m4rw3r@gmail.com>
2020-04-26 14:31:03 +0200

    feat: speed up unicode input

--- users/m4rw3r/config.h
+++ users/m4rw3r/config.h
@@ -4,6 +4,8 @@
 // We want to be able to do ESC : quickly, this will ignore the CTRL fucntion
 // of ESC if we release it before we release the other button
 #define IGNORE_MOD_TAP_INTERRUPT
+// Speed up unicode input
+#define UNICODE_TYPE_DELAY 0
 
 // Global audio configuration
 #ifdef AUDIO_ENABLE
